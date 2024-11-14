108d7e2f6bdb262565699072f70b9dbc192713c4
ksrawr <kenneth.surban62@gmail.com>
2024-11-13 22:27:33 -0800

    adds UI preview for drawing boxes

--- src/DrawBoxes.js
+++ src/DrawBoxes.js
@@ -11,46 +11,25 @@ const DrawBoxes = ({children, enabled}) => {
     /* 
     TODO: 
     - Delete the box
-    - Draw multiple boxes
     */
-        
-    const [startCoordinates, setStartCoordinates] = useState(null);
-    const [finalState, setFinalState] = useState(null);
     const [coordinates, setCoordinates] = useState(null);
     const [boxes, setBoxes] = useState([]);
+    const [showPreview, setShowPreview] = useState(false);
+    const [previewCoordinates, setPreviewCoordinates] = useState(initialState);
 
     const handleDraw = (e) => {
-        // if(!startCoordinates) {
-        //     const x = e.pageX;
-        //     const y = e.pageY;
-        //     setStartCoordinates({x, y});
-        // } else {
-        //     const boxObject = {
-        //         startX: startCoordinates.x,
-        //         startY: startCoordinates.y,
-        //         endX: e.pageX,
-        //         endY: e.pageY,
-        //     }
-        //     setFinalState(boxObject);
-        // }
-
         const { startX, startY } = coordinates || {};
         const x = e.pageX;
         const y = e.pageY;
         if(!(startX && startY)) {
             setCoordinates((prev) => ({...prev, startX: x, startY: y}));
+            setPreviewCoordinates((prev) => ({...prev, startX: x, startY: y}));
         } else {
             setCoordinates((prev) => ({...prev, endX: x, endY: y}));
+            setShowPreview(false);
+            setPreviewCoordinates(initialState);
         }
     };
-
-    useEffect(() => {
-        if(startCoordinates) console.log(startCoordinates);
-    }, [startCoordinates]);
-
-    useEffect(() => {
-        if(finalState) console.log(finalState);
-    }, [finalState]);
 
     useEffect(() => {
         console.log(boxes);
@@ -61,23 +40,29 @@ const DrawBoxes = ({children, enabled}) => {
         if(startX && startY && endX && endY) {
             setBoxes((prev) => [...prev, coordinates]);
             setCoordinates(initialState);
+        } else if(startX && startY) {
+            setShowPreview(true);
         }
     }, [coordinates]);
 
-    const displayBox = () => {
-        // const { startX, startY, endX, endY } = finalState;
-        // const boxStyle = {
-        //     height: `${endY - startY}px`,
-        //     width: `${endX - startX}px`,
-        //     position: "absolute",
-        //     border: "1px solid red",
-        //     left: `${startX}px`,
-        //     top: `${startY}px`,
-        // };
-        // return (
-        //     <div style={boxStyle}></div>
-        // )
+    const handleOnMouseEvent = (e) => {
+        setPreviewCoordinates((prev) => ({...prev, endX: e.pageX, endY: e.pageY }));
+    };
 
+    const displayPreview = () => {
+        const { startX, startY, endX, endY } = previewCoordinates || {};
+        const boxStyle = {
+            height: `${endY - startY}px`,
+            width: `${endX - startX}px`,
+            position: "absolute",
+            border: "1px solid red",
+            left: `${startX}px`,
+            top: `${startY}px`,
+        };
+        return <div style={boxStyle}></div>
+    };
+
+    const displayBox = () => {
         return boxes.map((b, i) => {
             const { startX, startY, endX, endY } = b || {};
             const boxStyle = {
@@ -94,12 +79,13 @@ const DrawBoxes = ({children, enabled}) => {
 
     if (enabled) {
         return (
-            <div style={{height: "100%", width: "100%"}} onClick={handleDraw}>
+            <div style={{height: "100%", width: "100%"}} onClick={handleDraw} onMouseMove={ setShowPreview ? handleOnMouseEvent : null}>
                 {boxes.length && displayBox()}
                 {/* Draw Children */}
                 <div className="container full">
                     {children}
                 </div>
+                {showPreview && displayPreview()}
             </div>
         )
     } else {
